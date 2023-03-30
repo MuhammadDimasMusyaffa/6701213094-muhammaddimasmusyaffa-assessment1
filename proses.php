c494021d508edb142a28926a3e8f5baf89365a7e
MuhammadDimasMusyaffa <muhdimasm96@gmail.com>
2023-03-30 09:56:17 +0700

    v1.3 buat proses input dan operasi aritmatika dengan if-else

---
+++ proses.php
@@ -0,0 +1,15 @@
+<?php
+  $angka1 = $_POST['angka1'];
+  $angka2 = $_POST['angka2'];
+  $operator = $_POST['operator'];
+
+  if ($operator == '+') {
+    $hasil = $angka1 + $angka2;
+  } elseif ($operator == '-') {
+    $hasil = $angka1 - $angka2;
+  } elseif ($operator == '*') {
+    $hasil = $angka1 * $angka2;
+  }
+
+  echo "Hasil operasi aritmatika $angka1 $operator $angka2 = $hasil";
+?>
